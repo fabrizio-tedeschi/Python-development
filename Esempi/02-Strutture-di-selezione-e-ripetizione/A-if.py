c35602c1756fcf2b598641f7e8750e2612f61a4b
Frizio03 <fabrizio.tedeschi.scuola@gmail.com>
2024-06-01 18:50:39 +0200

    Update esempi

---
+++ Esempi/02-Strutture-di-selezione-e-ripetizione/A-if.py
@@ -0,0 +1,10 @@
+#Input di un valore numerico
+x = int(input("Inseire un valore numerico: "))
+
+#Controllo del valore nuemerico
+if(x > 0):
+    print("Il numero", x, "risulta POSITIVO!")
+elif(x < 0):
+    print("Il numero", x, "risulta NEGATIVO!")
+else:
+    print("Il numero", x, "risulta NULLO!")
---
+++ Esempi/02-Strutture-di-selezione-e-ripetizione/B-for.py
@@ -0,0 +1,11 @@
+print("Stampa dei primi 10 numeri naturali:")
+
+for num in range(1,11):
+    print("La variabile num vale", num)
+print()
+
+print("Stampa ripetuta dello stesso testo:")
+for i in range(0, 5):
+    print("Riga", i, "stesso testo.")
+    print("Operazione...")
+print()
